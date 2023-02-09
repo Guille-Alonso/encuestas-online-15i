2b0ccd817b6112bf8c20136561b07c6cf1c8acd4
Nicolas <nicobenpe@gmail.com>
2023-02-08 21:44:32 -0300

    agregando nav

--- src/components/common/MainNavBar/MainNavBar.jsx
+++ src/components/common/MainNavBar/MainNavBar.jsx
@@ -1,8 +1,24 @@
-const MainNavBar = () => {
-    return ( 
-    <h1>Soy un navbar</h1> 
-    );
+import Nav from 'react-bootstrap/Nav';
+
+function MainNavBar() {
+  return (
+    <>
+
+    <Nav className="justify-content-end" activeKey="/home">
+        <Nav.Item>
+            <Nav.Link href="/home">Login</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link href='./RegisterForm'>Registro</Nav.Link>
+        </Nav.Item>
+    </Nav>
+    </>
+);
 }
 
 export default MainNavBar;
+
+
+
+
 
