888c6315e699d762b298075a1cb29e21fcdb9b43
guille alonso <guilloalonsot@gmail.com>
2023-02-14 09:28:48 -0300

    adminpage crud con LS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Layout from "./components/Layout/Layout"
+import AdminPage from './pages/AdminPage'
 import HomePage from './pages/HomePage'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Layout>
       <Routes>
         <Route path='/*' element={<HomePage/>}/>
+        <Route path='/admin' element={<AdminPage/>}/>
       </Routes>
       </Layout>
     </Router>
