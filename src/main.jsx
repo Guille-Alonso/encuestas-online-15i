feaab640b75eeb276683c4094e7506f16f214ff8
Stella Agüero <agu_stella@hotmail.com>
2023-02-18 13:34:08 -0300

    cambios

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.css'
+import React from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export const MainNavBar = () =>{
+
+
+    const {state} = useLocation()
+    const navigate = useNavigate()
+
+    console.log(state);
+
+    const onLogout = () => {
+
+        navigate("/Login", {
+            replace: true,
+        })
+
+
+    }
+
+    return (
+        <>
+            <Navbar className="Navbar" variant="dark">
+                
+            <Container className="mw-100 w-100 d-flex justify-content-between m-0">
+            <Navbar.Brand className="brand-logo">
+            <img className="mh-100 mw-100 d-block" alt="Logo" />
+            </Navbar.Brand>
+            <Navbar.Collapse className="justify-content-end">
+
+                {state?.logged ? (
+                    <div className='user'>
+                    <span className='username'>{state?.name}</span>
+                    <button className='btn-logOut' onClick={onLogout}>Cerrar Sesiòn</button>
+                </div>
+
+                    ) : (
+                    <nav>
+                    <Link to="/Login">Iniciar Sesiòn</Link>
+                    <Link to="/register">Registrarse</Link>
+                </nav>
+                    )
+                }
+
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+        
+        </>
+    )
+
+
+}
+
+export default MainNavBar;
