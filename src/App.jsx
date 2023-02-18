c63404c44806e61788e9756bd164c13171b28f1d
Stella Agüero <agu_stella@hotmail.com>
2023-02-18 14:07:07 -0300

    loginpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Layout from "./components/Layout/Layout"
-import AdminPage from './pages/AdminPage'
 import HomePage from './pages/HomePage'
-import { RegisterPage } from './pages/RegisterPage'
 import LoginPage from './pages/Login/LoginPage'
 import ForgetPassword from './pages/Login/Forgetpassword'
 
@@ -13,11 +11,9 @@ function App() {
       <Layout>
       <Routes>
         <Route path='/*' element={<HomePage/>}/>
-        <Route path='/register' element={<RegisterPage/>}/>
         <Route path='/Login' element={<LoginPage/>}/>
         <Route path='/ForgetPassword' element={<ForgetPassword/>}/>
-        <Route path='/admin' element={<AdminPage/>}/>
-      </Routes>
+        </Routes>
       </Layout>
     </Router>
   )
