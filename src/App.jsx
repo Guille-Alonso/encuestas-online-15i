dc282fc84932f0f4030670acebf6dfb50ceefc89
guille alonso <guilloalonsot@gmail.com>
2023-03-03 12:45:15 -0300

    quitando importacion que no existía

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/common/Layout/Layout";
 import  Content  from "./components/Content";
-/*import { Layout } from "./components/LayoutHome/LayoutHome";*/
-import HomePage from "./pages/HomePage";
 import AboutUs from "./pages/AboutUs";
 import {RegisterPage} from "./pages/RegisterPage";
 import  LoginPage  from "./pages/Login/LoginPage";
